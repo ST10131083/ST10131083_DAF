e5f218768538f520bb457bf204e979561d78d64a
ST10131083 <ST10131083@rcconnect.edu.za>
2022-10-11 09:06:02 +0200

    Add database tables to migrations

---
+++ ST10131083_DAF/Migrations/20221011070124_user.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ST10131083_DAF.Migrations
+{
+    public partial class user : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    isActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Disasters",
+                columns: table => new
+                {
+                    DisasterId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DisasterName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DisasterType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AmountAllocation = table.Column<double>(type: "float", nullable: false),
+                    Categoryid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Disasters", x => x.DisasterId);
+                    table.ForeignKey(
+                        name: "FK_Disasters_Categories_Categoryid",
+                        column: x => x.Categoryid,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Donations",
+                columns: table => new
+                {
+                    Donationid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<double>(type: "float", nullable: false),
+                    DisasterName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Lname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mobile = table.Column<long>(type: "bigint", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    isPrivate = table.Column<bool>(type: "bit", nullable: false),
+                    DisasterId = table.Column<int>(type: "int", nullable: false),
+                    Userid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donations", x => x.Donationid);
+                    table.ForeignKey(
+                        name: "FK_Donations_Disasters_DisasterId",
+                        column: x => x.DisasterId,
+                        principalTable: "Disasters",
+                        principalColumn: "DisasterId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Donations_Users_Userid",
+                        column: x => x.Userid,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Good",
+                columns: table => new
+                {
+                    Goodsid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumberofGoods = table.Column<int>(type: "int", nullable: false),
+                    DisasterName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Lname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mobile = table.Column<long>(type: "bigint", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    isPrivate = table.Column<bool>(type: "bit", nullable: false),
+                    DisasterId = table.Column<int>(type: "int", nullable: false),
+                    Userid = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Good", x => x.Goodsid);
+                    table.ForeignKey(
+                        name: "FK_Good_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Good_Disasters_DisasterId",
+                        column: x => x.DisasterId,
+                        principalTable: "Disasters",
+                        principalColumn: "DisasterId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Good_Users_Userid",
+                        column: x => x.Userid,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DisasterAllocations",
+                columns: table => new
+                {
+                    DisasterAllocationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DisasterName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AmountDonated = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GoodsDonated = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AmountGoal = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DisasterId = table.Column<int>(type: "int", nullable: true),
+                    Donationid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DisasterAllocations", x => x.DisasterAllocationId);
+                    table.ForeignKey(
+                        name: "FK_DisasterAllocations_Disasters_DisasterId",
+                        column: x => x.DisasterId,
+                        principalTable: "Disasters",
+                        principalColumn: "DisasterId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_DisasterAllocations_Donations_Donationid",
+                        column: x => x.Donationid,
+                        principalTable: "Donations",
+                        principalColumn: "Donationid",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DisasterAllocations_DisasterId",
+                table: "DisasterAllocations",
+                column: "DisasterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DisasterAllocations_Donationid",
+                table: "DisasterAllocations",
+                column: "Donationid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Disasters_Categoryid",
+                table: "Disasters",
+                column: "Categoryid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Donations_DisasterId",
+                table: "Donations",
+                column: "DisasterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Donations_Userid",
+                table: "Donations",
+                column: "Userid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Good_CategoryId",
+                table: "Good",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Good_DisasterId",
+                table: "Good",
+                column: "DisasterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Good_Userid",
+                table: "Good",
+                column: "Userid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DisasterAllocations");
+
+            migrationBuilder.DropTable(
+                name: "Good");
+
+            migrationBuilder.DropTable(
+                name: "Donations");
+
+            migrationBuilder.DropTable(
+                name: "Disasters");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ ST10131083_DAF/Migrations/20221011070314_donation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ST10131083_DAF.Migrations
+{
+    public partial class donation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
